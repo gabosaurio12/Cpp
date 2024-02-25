4601e33771adbafc6963016799d3bd745754140c
Gabosaurio <gabgonlo05@gmail.com>
2024-02-24 23:56:29 -0600

    Feat: New program 'Encontrar Cadena' and deleted .exe

---
+++ Ejercicios/Encontrar Cadena/encontrarCadena.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int longitudCad(string cad){
+	int i = 0;
+	while(cad[i] != '\0')
+		i++;
+
+	return i;
+}
+
+
+void insertar(string add, int pos, string cadena){
+	
+}
+
+
+void encontrarCad(string cadI, string cadB){
+	int longCadI = longitudCad(cadI);	int band = 1;
+	for(int i = 0; i < longCadI-1; i++){
+		if(cadI[i] == cadB[0]){
+			band = 1;
+			int j = 1;	int k = i + 1;	int longCadB = longitudCad(cadB);
+
+			while(band && j < longCadB){
+				if(cadI[k] != cadB[j]){
+					band = 0;
+				}
+				k++;
+				j++;
+			}
+
+			if(band){
+				/*insertar('*',i,cadI);
+				insertar('*',k+1,cadI);
+				*/
+				cout << cadI << endl;
+			}	
+		}
+	}
+
+	if(not band)
+		cout << "No se encontro la Cadena Buscada \n";
+}
+
+int main(){
+	string cad1, cad2;
+	
+	cout << "Ingresa la Cadena Base y la Cadena que quieres Buscar \n";
+	cin >> cad1 >> cad2;
+
+	encontrarCad(cad1,cad2);
+
+	return 0;
+}
