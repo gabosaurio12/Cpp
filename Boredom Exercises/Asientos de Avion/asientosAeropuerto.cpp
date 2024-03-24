b85e7398788d521f28829411135eefbdd2779471
GaboWinDesk <gabgonlo05@gmail.com>
2024-03-24 17:16:52 -0600

    Fix asientosAeropuerto.cpp, apartado fixed with recursivity

--- Boredom Exercises/Asientos de Avion/asientosAeropuerto.cpp
+++ Boredom Exercises/Asientos de Avion/asientosAeropuerto.cpp
@@ -37,11 +37,64 @@ void imprimirAsientos(int mA[][5]){
     }
 }
 
+int apartar(int mA[][5], string asiento){
+    int l = int(asiento[0]) - 64;
+    int n = int(asiento[1]) - 48;
+
+    if (mA[l][n] == 79){
+        cout << "Ingresa un asiento vacio ";
+        cout << "O ingresa 's' para salir \n";
+        cin >> asiento;
+
+        if (asiento == "s" || asiento == "S")
+            return 0;
+
+        else
+            return apartar(mA, asiento);
+    }
+
+    else{
+        mA[l][n] = 79;
+        return 1;
+    }
+
+    return 0;
+}
+
 int main(){
     int matAsientos[8][5];
+    string asientoAp;
 
     llenado(matAsientos);
     imprimirAsientos(matAsientos);
 
+    int band = 1;
+    int c = 0;
+    while (band){
+        cout << "Ingresa el asiento que deseas apartar \n";
+        cin >> asientoAp;
+
+        if (apartar(matAsientos, asientoAp)){
+            cout << "Tu asiento ha sido apartado exitosamente \n";
+            imprimirAsientos(matAsientos);
+            c++;
+        }
+        else{
+            return 0;
+        }
+
+        if (c == 28)
+            cout << "El vuelo esta lleno \n";
+            return 0;
+
+        else{
+            char sn;
+            cout << "Deseas leer otro asiento? s/n \n";
+            cin >> sn;
+            if (sn == 'n' || sn == 'N')
+                band = 0;
+        }
+    }
+    
     return 0;
 }
